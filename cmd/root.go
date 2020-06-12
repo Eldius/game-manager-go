41dd4974c2212b9ddfa00795740a7f6f0cb3b8d1
Eldio Santos <eldio.santos@m4u.com.br>
2020-06-11 23:42:34 -0300

    Fixed the root command description...

--- cmd/root.go
+++ cmd/root.go
@@ -19,13 +19,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "game-manager-go",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A simple tool to manage game servers",
+	Long:  `A simple tool to manager game servers.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
